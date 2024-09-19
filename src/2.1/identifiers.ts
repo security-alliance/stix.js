dd7826b410431593cda6193050b6ce7f9cb5e4a0
samczsun <samczsun@users.noreply.github.com>
2024-09-19 14:48:26 -0700

    update identifiers

--- src/2.1/identifiers.ts
+++ src/2.1/identifiers.ts
@@ -6,14 +6,22 @@ export const OASIS_NAMESPACE = "00abedb4-aa42-466c-9c01-fed23315a9b7";
 
 export type Any<T> = T & Record<any, any>;
 
-export const generateId = <T extends StixObjectType>(type: T, props: any): Identifier<T> => {
-    return `${type}--${v5(canonicalize(props), OASIS_NAMESPACE)}`;
+export const generateRandomId = <T extends StixObjectType>(type: T): Identifier<T> => {
+    return `${type}--${v4()}`;
+};
+
+export const generateDeterministicId = <T extends StixObjectType>(
+    type: T,
+    props: any,
+    namespace: string = OASIS_NAMESPACE,
+): Identifier<T> => {
+    return `${type}--${v5(canonicalize(props), namespace)}`;
 };
 
 export const generateDomainObservableId = (props: Any<{ value: string }>): Identifier<"domain-name"> => {
-    return generateId("domain-name", { value: props.value });
+    return generateDeterministicId("domain-name", { value: props.value });
 };
 
 export const generateBundleId = (): Identifier<"bundle"> => {
-    return `bundle--${v4()}`;
+    return generateRandomId("bundle");
 };
